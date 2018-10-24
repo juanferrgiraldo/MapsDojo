196dde272ed196f2908cc6ed57d2790fff3e2de2
juanferrgiraldo <juanferrgiraldo@gmail.com>
2018-10-24 00:50:28 -0500

    Get current user location.

--- app/src/main/java/com/example/juanfer/mapsdojo/MapsActivity.kt
+++ app/src/main/java/com/example/juanfer/mapsdojo/MapsActivity.kt
@@ -1,6 +1,7 @@
 package com.example.juanfer.mapsdojo
 
 import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
@@ -12,12 +13,12 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.Marker
-import com.google.android.gms.maps.model.MarkerOptions
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
     private lateinit var mMap: GoogleMap
     private lateinit var fusedLocationClient: FusedLocationProviderClient  //Determine device location.
+    private lateinit var lastLocation: Location
 
     companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1
@@ -48,10 +49,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true
         mMap.setOnMarkerClickListener(this)
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 9f))
         setUpMap()
     }
 
@@ -61,6 +58,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMarker
             ActivityCompat.requestPermissions(this,     //If not request from the user.
                 arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
             return
+        }
+        mMap.isMyLocationEnabled = true  //draw nd center the map on users location
+
+        fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->  //gives the most recent location available
+            if (location != null) {
+                lastLocation = location
+                val currentLatLng = LatLng(location.latitude, location.longitude)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            }
         }
     }
 }
